877807c158aafc6b128249dba8d2dab15989be2d
Ahmet Okay <ahmetokay@users.noreply.github.com>
2021-01-11 20:28:54 +0300

    Add files via upload

--- component/User.js
+++ component/User.js
@@ -1,6 +1,7 @@
 import React, {useEffect, useState, useRef } from 'react';
 import {DataTable} from 'primereact/datatable';
 import {Column} from 'primereact/column';
+import {Dialog} from 'primereact/dialog';
 import {Button} from 'primereact/button';
 import Axios from 'axios';
 import 'primereact/resources/themes/saga-blue/theme.css';
@@ -17,8 +18,48 @@ function User() {
     const [surname, setSurname] = useState('');
     const [userList, setUserList] = useState([]);
 
+    const [displayConfirmation, setDisplayConfirmation] = useState(false);
+    const [rowId, setRowId] = useState(0);
+
+    const onClick = (name, rowId) => {
+        setDisplayConfirmation(true)
+        setRowId(rowId)
+    }
+
+    const onHide = (name) => {
+        setDisplayConfirmation(false)
+    }
+
+    const sil = (id) => {
+
+        console.log(id)
+        debugger
+
+        Axios.get('http://localhost:5050/delete', {
+            params: {
+                "id": rowId
+            }
+        }).then((response) => {
+            // console.log(response)
+            showSuccess()
+        })
+    }
+
+    const renderFooter = (name, rowId) => {
+        return (
+            <div>
+                <Button label="No" icon="pi pi-times" onClick={() => onHide(name)} className="p-button-text" />
+                <Button label="Yes" icon="pi pi-check" onClick={() => {onHide(name); sil(rowId)}} autoFocus />
+            </div>
+        );
+    }
+
     const showSuccess = () => {
         toast.current.show({severity:'success', summary: 'Success Message', detail:'Message Content', life: 3000});
+    }
+
+    const showFail = () => {
+        toast.current.show({severity:'error', summary: 'Error Message', detail:'Message Content', life: 3000});
     }
 
     const kaydet = () => {
@@ -31,15 +72,18 @@ function User() {
         Axios.post('http://localhost:5050/add', data).then((response) => {
             // console.log(response)
             showSuccess()
+            showFail()
         })
     }
 
-    const editColumn = () => {
+
+
+    const editColumn = (rowData) => {
 
         return (
             <div>
                 <Button icon="pi pi-refresh" className="p-button-rounded p-button-edit p-button-text" />
-                <Button icon="pi pi-times" className="p-button-rounded p-button-danger p-button-text" />
+                <Button icon="pi pi-times" className="p-button-rounded p-button-danger p-button-text" onClick={() => onClick('displayConfirmation')} />
             </div>
         )
     }
@@ -50,12 +94,19 @@ function User() {
             // console.log(response)
             setUserList(response.data)
         })
-    });
+    }, []);
 
 
     return (
         <div>
             <Toast ref={toast} />
+
+            <Dialog header="Confirmation" visible={displayConfirmation} modal style={{ width: '350px' }} footer={renderFooter('displayConfirmation', rowId)} onHide={() => onHide('displayConfirmation')}>
+                <div className="confirmation-content">
+                    <i className="pi pi-exclamation-triangle p-mr-3" style={{ fontSize: '2rem' }} />
+                    <span>Are you sure you want to proceed?</span>
+                </div>
+            </Dialog>
 
             <form onSubmit={kaydet}>
 
